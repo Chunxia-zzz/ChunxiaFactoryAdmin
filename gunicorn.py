16547ebf9ab3d0bf7cff99c8802fd8f9abe0922f
Chunxia <1317214963@qq.com>
2022-11-19 16:28:48 +0800

    update port

--- gunicorn.py
+++ gunicorn.py
@@ -4,7 +4,7 @@ import multiprocessing
 
 # debug = True
 loglevel = 'debug'
-bind = "0.0.0.0:8080"
+bind = "0.0.0.0:8081"
 pidfile = "logs/gunicorn.pid"
 accesslog = "logs/access.log"
 errorlog = "logs/error.log"
